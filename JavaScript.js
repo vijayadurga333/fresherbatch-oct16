27a563bd4543b5963f34dc7f15773646dc95519b
vijayadurga333 <115156534+vijayadurga333@users.noreply.github.com>
2022-10-09 22:03:14 +0530

    added text

---
+++ JavaScript.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Demo JavaScript in Body</h2>
+<h4>This is sample page</h4>
+
+<p id="demo">A Paragraph.</p>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+</script>
+
+</body>
+</html> 
